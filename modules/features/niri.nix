d350aa3b0af73ba441bdb65df34ae06f53929294
Arthur Barbosa Azevedo <52844186+Arthaaas@users.noreply.github.com>
2026-04-25 14:41:00 -0300

    Atualiza erro de digitação

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -21,7 +21,7 @@
         binds = {
           # Apps
           "Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
-          "Mod+Q".close-window = null;
+          "Mod+Q".close-window = {};
           "Mod+S".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
         
           # Foco entre janelas
