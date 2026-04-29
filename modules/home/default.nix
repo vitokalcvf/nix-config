a3281484653f91d00de53d23135594f054781d40
Arthaaas <iarthu0@gmail.com>
2026-04-28 21:33:48 -0300

    feat: troca os ícones padrão e o cursor do mouse

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -3,8 +3,23 @@
     home-manager = {
       useGlobalPkgs = true;
       useUserPackages = true;
+      backupFileExtension = "hm-backup";
       users.arthas = {
         home.stateVersion = "25.11";
+        gtk = {
+          enable = true;
+          iconTheme = {
+            name = "Papirus-Dark";
+            package = pkgs.papirus-icon-theme;
+          };
+        };
+        home.pointerCursor = {
+          name = "breeze_cursors";
+          package = pkgs.kdePackages.breeze;
+          size = 24;
+          gtk.enable = true;
+          x11.enable = true;
+        };
         home.file.".config/kitty/tmux-main.session".source = ../dotfiles/kitty/tmux-main.session;
         programs.kitty = {
           enable = true;
