f97d79343fc840602268faa3b3ad6ab704cac0fc
Arthaaas <iarthu0@gmail.com>
2026-05-08 13:48:25 -0300

    feat: adiciona configurações do segundo monitor

--- data/desktop-casa-display-settings.nix
+++ data/desktop-casa-display-settings.nix
@@ -1,8 +1,27 @@
 {
-  output = _: {
-    props = "DP-5";
-    content = {
+  outputs = {
+    "DP-5" = {
       mode = "1920x1080@180.003";
+      position = _: {
+        props = {
+          x = 0;
+          y = 912;
+        };
+      };
+      scale = 1.0;
+      transform = "normal";
+    };
+
+    "HDMI-A-2" = {
+      mode = "1920x1080@60.000";
+      position = _: {
+        props = {
+          x = 1920;
+          y = 912;
+        };
+      };
+      scale = 1.0;
+      transform = "normal";
     };
   };
 }
