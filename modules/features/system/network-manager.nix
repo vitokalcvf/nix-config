c77653fd72b6e1ac41ea18d1bfa49a19c8178ef7
vitokalcvf <vitorlcvf@gmail.com>
2026-06-18 11:01:27 -0300

    feat(network-manager): add OpenVPN plugin support to NetworkManager

--- modules/features/system/network-manager.nix
+++ modules/features/system/network-manager.nix
@@ -1,10 +1,13 @@
 { self, inputs, ... }:
 let
   networkModule =
-    { config, ... }:
+    { config, pkgs, ... }:
     {
       networking.hostName = config.my.host.name;
       networking.networkmanager.enable = true;
+      networking.networkmanager.plugins = with pkgs; [
+        networkmanager-openvpn
+      ];
       networking.firewall.enable = true;
     };
 in
