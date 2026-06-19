571c1ae64727b71ad060bb51551d7811102b7dae
vitokalcvf <vitorlcvf@gmail.com>
2026-06-19 13:48:09 -0300

    refactor(ssh): remove commented explanations for clarity in ssh configuration

--- modules/home/programs/ssh.nix
+++ modules/home/programs/ssh.nix
@@ -5,14 +5,11 @@
     {
       programs.ssh = {
         enable = true;
-        # A partir do home-manager recente, os defaults globais (Host *) sao
-        # opt-in. Desativamos para manter o config enxuto e explicito.
         enableDefaultConfig = false;
 
         matchBlocks."github.com" = {
           user = "git";
           identityFile = "~/.ssh/id_ed25519";
-          # So usa a chave declarada acima, sem oferecer outras do agent.
           identitiesOnly = true;
         };
 
